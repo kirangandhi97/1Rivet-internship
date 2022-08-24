9ca3b995223070fbb88a2214408988f9b20e1f7d
Kiran Gandhi <kiran.gandhi@1Rintern.local>
2022-08-24 12:21:23 +0530

    daily tasks-challenges

---
+++ example1/assets/JS/script.js
@@ -0,0 +1,18 @@
+function multiP(x,y){
+ return x*y;
+}
+console.log(multiP(5,6));
+// console.log(x);
+
+let mul = (x,y) => x*y;
+console.log(mul(1,10));
+
+// for of loop 
+let data = ["abc", "kiran", "hiral"]
+let text ="";
+for(let x of data){
+text += x + "<br>";
+}
+document.write(text);
+console.log(text);
+
