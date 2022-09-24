34ee321fb60431620d2208dace69cbe01a9cde4f
kiran gandhi <kirangandhi97@gmail.com>
2022-09-24 14:53:44 +0530

    without the service

--- Angular/Angular-Json/jsonObservable/src/app/employee/employee-list/employee-list.component.ts
+++ Angular/Angular-Json/jsonObservable/src/app/employee/employee-list/employee-list.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { EmployeeService } from 'src/app/services/employee.service';
 
@@ -8,11 +9,18 @@ import { EmployeeService } from 'src/app/services/employee.service';
 })
 export class EmployeeListComponent implements OnInit {
 ListData:any;
-  constructor(private UserListData:EmployeeService) { }
+  constructor(private UserListData:EmployeeService, private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.UserListData.getUSer().subscribe((data) => this.ListData = data)
-    console.log(this.ListData);
+    // this.UserListData.getUSer().subscribe((data) => this.ListData = data)
+    // console.log(this.ListData);
+    this.ListData=[];
+    this.getUserData();
   }
 
+  getUserData(){
+    this.http.get('http://localhost:3000/employee').subscribe((result:any) =>
+    this.ListData = result
+    )
+  }
 }
